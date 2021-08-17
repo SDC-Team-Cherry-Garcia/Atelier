d00f447e87bd316a48f54fcf48806bf8d422971f
Yuki Li <yunqili1025@gmail.com>
2021-08-17 09:12:33 -0400

    scaling with cluster server

--- server/index.js
+++ server/index.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const db = require('../db-psql');
+//add load balancer
+const cluster = require('cluster')
+const numCPUs = require('os').cpus().length;
 
 const app = express();
 const PORT = 3000;
@@ -7,95 +10,112 @@ const PORT = 3000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get('/qa/questions', (req, res) => {
-  console.log('REQ PARAM ', req.params);
-  console.log('REQ BODY ', req.body);
-  console.log('REQ QUERY ', req.query);
-  db.getQsByProductId(req.query.product_id, req.query.page||1, req.query.count||5, (err, result) => {
-    if (err) {
-      console.log('failed to get Qs from server');
-      res.sendStatus(404);
-    }
-    res.status(200).json(result);
-  })
-});
+if (cluster.isMaster) {
+  console.log(`Master ${process.pid} is running`);
+  for (let i = 0; i < numCPUs; i++) {
+    cluster.fork();
+  }
+  cluster.on('exit', (worker, code, signal) => {
+    console.log(`worker ${worker.process.pid} died`);
+  });
+} else {
+  app.listen(PORT, err => {
+    err ?
+      console.log("Error in server setup") :
+      console.log(`Worker ${process.pid} started`);
+  });
+  app.get('/qa/questions', (req, res) => {
+    // console.log('REQ PARAM ', req.params);
+    // console.log('REQ BODY ', req.body);
+    // console.log('REQ QUERY ', req.query);
+    db.getQsByProductId(req.query.product_id, req.query.page||1, req.query.count||5, (err, result) => {
+      if (err) {
+        console.log('failed to get Qs from server');
+        res.sendStatus(404);
+      }
+      res.status(200).json(result);
+    })
+  });
 
-app.get('/qa/questions/:question_id/answers', (req, res) => {
-  console.log('REQ QUERY ', req.query);
-  console.log('REQ PARAM ', req.params);
-  db.getAnsByQId(req.params.question_id, req.query.page||1, req.query.count||5, (err, result) => {
-    if (err) {
-      console.log('failed to get answer list from server');
-      res.sendStatus(404);
-    }
-    res.status(200).json(result);
-  })
-});
+  app.get('/qa/questions/:question_id/answers', (req, res) => {
+    console.log('REQ QUERY ', req.query);
+    console.log('REQ PARAM ', req.params);
+    db.getAnsByQId(req.params.question_id, req.query.page||1, req.query.count||5, (err, result) => {
+      if (err) {
+        console.log('failed to get answer list from server');
+        res.sendStatus(404);
+      }
+      res.status(200).json(result);
+    })
+  });
 
-app.post('/qa/questions', (req, res) => {
-  console.log('REQ BODY ', req.body);
-  db.addQ(req.body, (err, result) => {
-    if (err) {
-      console.log('failed to post newq to server');
-      res.sendStatus(404);
-    }
-    res.status(201).send('Created');
-  })
-});
+  app.post('/qa/questions', (req, res) => {
+    console.log('REQ BODY ', req.body);
+    db.addQ(req.body, (err, result) => {
+      if (err) {
+        console.log('failed to post newq to server');
+        res.sendStatus(404);
+      }
+      res.status(201).send('Created');
+    })
+  });
 
-app.post('/qa/questions/:question_id/answers', (req, res) => {
-  console.log('REQ BODY ', req.body);
-  db.addAns(req.params.question_id, req.body, (err, result) => {
-    if (err) {
-      console.log('failed to post new Answer to server');
-      res.sendStatus(404);
-    }
-    res.status(201).send('Created');
-  })
-});
+  app.post('/qa/questions/:question_id/answers', (req, res) => {
+    console.log('REQ BODY ', req.body);
+    db.addAns(req.params.question_id, req.body, (err, result) => {
+      if (err) {
+        console.log('failed to post new Answer to server');
+        res.sendStatus(404);
+      }
+      res.status(201).send('Created');
+    })
+  });
 
-app.put('/qa/questions/:question_id/helpful', (req, res) => {
-  db.markQhelpfull(req.params.question_id, (err, result) => {
-    if (err) {
-      console.log('failed to mark Q helpful via server');
-      res.sendStatus(404);
-    }
-    res.sendStatus(204);
-  })
-});
+  app.put('/qa/questions/:question_id/helpful', (req, res) => {
+    db.markQhelpfull(req.params.question_id, (err, result) => {
+      if (err) {
+        console.log('failed to mark Q helpful via server');
+        res.sendStatus(404);
+      }
+      res.sendStatus(204);
+    })
+  });
 
-app.put('/qa/answers/:answer_id/helpful', (req, res) => {
-  db.markAhelpful(req.params.answer_id, (err, result) => {
-    if (err) {
-      console.log('failed to mark A helpful via server');
-      res.sendStatus(404);
-    }
-    res.sendStatus(204);
-  })
-});
+  app.put('/qa/answers/:answer_id/helpful', (req, res) => {
+    db.markAhelpful(req.params.answer_id, (err, result) => {
+      if (err) {
+        console.log('failed to mark A helpful via server');
+        res.sendStatus(404);
+      }
+      res.sendStatus(204);
+    })
+  });
 
-app.put('/qa/questions/:question_id/report', (req, res) => {
-  db.reportQ(req.params.question_id, (err, result) => {
-    if (err) {
-      console.log('failed to report Q via server');
-      res.sendStatus(404);
-    }
-    res.sendStatus(204);
-  })
-})
+  app.put('/qa/questions/:question_id/report', (req, res) => {
+    db.reportQ(req.params.question_id, (err, result) => {
+      if (err) {
+        console.log('failed to report Q via server');
+        res.sendStatus(404);
+      }
+      res.sendStatus(204);
+    })
+  });
 
-app.put('/qa/answers/:answer_id/report', (req, res) => {
-  db.reportA(req.params.answer_id, (err, result) => {
-    if (err) {
-      console.log('failed to report A via server');
-      res.sendStatus(404);
-    }
-    res.sendStatus(204);
-  })
-})
+  app.put('/qa/answers/:answer_id/report', (req, res) => {
+    db.reportA(req.params.answer_id, (err, result) => {
+      if (err) {
+        console.log('failed to report A via server');
+        res.sendStatus(404);
+      }
+      res.sendStatus(204);
+    })
+  });
 
-app.listen(PORT, () => {
-  console.log(`listening on port ${PORT}`);
-});
+}
+
+
+// app.listen(PORT, () => {
+//   console.log(`listening on port ${PORT}`);
+// });
 
 
